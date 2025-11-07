7f3d0b6e6bfbcad1ee3c0aca28fca11694d1f55d
dev05 <hhpassarela@gmail.com>
2025-11-07 18:44:08 -0300

    Criação TarefaRepository.java

--- src/main/java/com/agibank/gerenciador_tarefas/repository/TarefaRepository.java
+++ src/main/java/com/agibank/gerenciador_tarefas/repository/TarefaRepository.java
@@ -1,4 +1,7 @@
 package com.agibank.gerenciador_tarefas.repository;
 
-public interface TarefaRepository {
+import com.agibank.gerenciador_tarefas.model.Tarefas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefaRepository extends JpaRepository<Tarefas, Long> {
 }
