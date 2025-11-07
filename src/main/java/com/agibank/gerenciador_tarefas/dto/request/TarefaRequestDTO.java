3045b13fe24493334e72386e5f0e4b7f4e30ed27
rafael.santos <job.rafaelsantos@gmail.com>
2025-11-07 20:34:31 -0300

    Criar TarefaRequestDTO

--- src/main/java/com/agibank/gerenciador_tarefas/dto/request/TarefaRequestDTO.java
+++ src/main/java/com/agibank/gerenciador_tarefas/dto/request/TarefaRequestDTO.java
@@ -1,4 +1,19 @@
 package com.agibank.gerenciador_tarefas.dto.request;
 
-public record TarefaRequestDTO() {
+import com.agibank.gerenciador_tarefas.model.enums.Situacao;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+
+public record TarefaRequestDTO(@NotBlank(message = "Titulo é obrigatório")
+                               String titulo,
+                               @NotBlank(message = "Descricao é obrigatorio")
+                               String descricao,
+                               @NotBlank(message = "Matricula é obrigatorio")
+                               String matricula,
+                               @NotBlank(message = "Situacao é obrigatorio")
+                               @Enumerated(EnumType.STRING)
+                               Situacao situacao) {
+
+
 }
