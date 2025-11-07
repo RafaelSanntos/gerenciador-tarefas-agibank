4035bf8b83d05a233830ed909effcbfa62036bfa
unknown <dev39@NB-UNIAGI-39.AGIPLAN.LOCAL>
2025-11-07 18:52:33 -0300

    Criado o UsuarioRepository

--- src/main/java/com/agibank/gerenciador_tarefas/repository/UsuarioRepository.java
+++ src/main/java/com/agibank/gerenciador_tarefas/repository/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package com.agibank.gerenciador_tarefas.repository;
 
-public interface UsuarioRepository {
+import com.agibank.gerenciador_tarefas.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
 }
