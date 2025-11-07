a5242bfc80344ea21539e1d8614097bbec72d1e6
biin4 <sabrinamorenopa@gmail.com>
2025-11-07 19:01:10 -0300

    adicionando tarefaResquest

--- src/main/java/com/agibank/gerenciador_tarefas/dto/request/TarefaRequestDTO.java
+++ src/main/java/com/agibank/gerenciador_tarefas/dto/request/TarefaRequestDTO.java
@@ -1,4 +1,21 @@
 package com.agibank.gerenciador_tarefas.dto.request;
 
+import com.agibank.gerenciador_tarefas.model.enums.Situacao;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+
 public class TarefaRequestDTO {
+    @NotBlank(message = "Titulo é obrigatório")
+    private String titulo;
+
+    @NotBlank(message = "Descricao é obrigatorio")
+    private String descricao;
+
+    @NotBlank(message = "Matricula é obrigatorio")
+    private String matricula;
+
+    @NotBlank(message = "Situacao é obrigatorio")
+    @Enumerated(EnumType.STRING)
+    private Situacao situacao;
 }
