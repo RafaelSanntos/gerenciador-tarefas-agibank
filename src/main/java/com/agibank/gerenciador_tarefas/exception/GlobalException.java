cddf2443495e401a8f206f3977052d06eff64bc3
dev05 <hhpassarela@gmail.com>
2025-11-07 22:42:27 -0300

    Criação classe de tratamento de exceção TarefasException.java e classe de tratamento de exceções GlobalException.java

---
+++ src/main/java/com/agibank/gerenciador_tarefas/exception/GlobalException.java
@@ -0,0 +1,22 @@
+package com.agibank.gerenciador_tarefas.exception.Global;
+
+import com.agibank.gerenciador_tarefas.exception.Tarefas.TarefasException;
+import com.agibank.gerenciador_tarefas.exception.Usuarios.UsuarioException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(TarefasException.class)
+    public ResponseEntity<String> handlerTarefasException(TarefasException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UsuarioException.class)
+    public ResponseEntity<String> handlerUsuarioException(UsuarioException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+}
--- src/main/java/com/agibank/gerenciador_tarefas/exception/Tarefas/TarefasException.java
+++ src/main/java/com/agibank/gerenciador_tarefas/exception/Tarefas/TarefasException.java
@@ -1,4 +1,7 @@
 package com.agibank.gerenciador_tarefas.exception.Tarefas;
 
-public class TarefasException {
+public class TarefasException extends RuntimeException {
+    public TarefasException(String message) {
+        super(message);
+    }
 }
