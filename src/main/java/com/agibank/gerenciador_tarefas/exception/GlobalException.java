0d281d3939ab09761b20819c5f6374b0c5635de3
dev05 <hhpassarela@gmail.com>
2025-11-08 02:32:38 -0300

    Fix: correção GlobalException.java

--- src/main/java/com/agibank/gerenciador_tarefas/exception/GlobalException.java
+++ src/main/java/com/agibank/gerenciador_tarefas/exception/GlobalException.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalException extends RuntimeException {
+public class GlobalException {
 
     @ExceptionHandler(TarefasException.class)
     public ResponseEntity<String> handlerTarefasException(TarefasException e) {
